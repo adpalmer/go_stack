e37fd86569f96dd0472344d33e8f1c5b2610e4db
Alex Palmer <alexpalmer90@gmail.com>
2013-03-15 22:46:06 -0400

    Initial Commit

---
+++ stack.go
@@ -0,0 +1,46 @@
+package stack
+
+import (
+	"errors"
+)
+
+type Stack struct {
+	top  *Element
+	size int
+}
+
+type Element struct {
+	value interface{}
+	next  *Element
+}
+
+func NewStack() *Stack {
+	return &Stack{top: nil, size: 0}
+}
+
+func (s *Stack) Len() int {
+	return s.size
+}
+
+func (s *Stack) Empty() bool {
+	if s.size != 0 {
+		return false
+	}
+	return true
+}
+
+func (s *Stack) Push(value interface{}) error {
+	s.top = &Element{value, s.top}
+	s.size++
+	return nil
+}
+
+func (s *Stack) Pop() (interface{}, error) {
+	if s.size > 0 {
+		val := s.top.value
+		s.top = s.top.next
+		s.size--
+		return val, nil
+	}
+	return 0, errors.New("Unable to Pop: Empty Stack")
+}
---
+++ stack_test.go
@@ -0,0 +1,82 @@
+package stack
+
+import "testing"
+
+func TestEmpty(t *testing.T) {
+	s := NewStack()
+	if s.Empty() == false {
+		t.Error("Stack should be empty")
+	}
+	s.Push(10)
+	if s.Empty() == true {
+		t.Error("Stack should not be empty")
+	}
+	s.Pop()
+	if s.Empty() == false {
+		t.Error("Stack should be empty")
+	}
+}
+
+func TestLen(t *testing.T) {
+	s := NewStack()
+
+	// Test that s.Len() is incremented as items pushed
+	for i := 0; i < 10; i++ {
+		if s.Len() != i {
+			t.Errorf("Stack length is %d instead of %d", i, s.Len())
+		}
+		s.Push(1)
+	}
+	// Test that s.Len() is decremented as items popped
+	for i := 10; i >= 0; i-- {
+		if s.Len() != i {
+			t.Errorf("Stack length is %d instead of %d", i, s.Len())
+		}
+		s.Pop()
+	}
+	// Test that s.Len() doesn't become negative when nothing to pop
+	s.Pop()
+	if s.Len() != 0 {
+		t.Errorf("Stack length is %d instead of 0", s.Len())
+	}
+}
+
+func TestPush(t *testing.T) {
+	s := NewStack()
+	err := s.Push(10)
+	if err != nil {
+		t.Errorf("Stack Push error: %s", err)
+	}
+	err = s.Push(11)
+	if err != nil {
+		t.Errorf("Stack Push error: %s", err)
+	}
+}
+
+func TestPop(t *testing.T) {
+	s := NewStack()
+	v1 := 10
+	v2 := 11
+
+	_, err := s.Pop()
+	if err == nil {
+		t.Errorf("stack.Pop() should have returned Empty Stack error")
+	}
+	s.Push(v1)
+	s.Push(v2)
+	val, err := s.Pop()
+	if err != nil {
+		t.Errorf("stack.Pop() returned error: %s", err)
+	}
+	if val != v2 {
+		t.Error("stack.Pop() returned %d but should have returned %d", val, v2)
+	}
+
+	val, err = s.Pop()
+	if err != nil {
+		t.Errorf("stack.Pop() returned error: %s", err)
+	}
+	if val != v1 {
+		t.Error("stack.Pop() returned %d but should have returned %d", val, v1)
+	}
+}
